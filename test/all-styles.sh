3072d7ce0646dbd743de1b0ab59f481c2e38d383
Hartmut Holzgraefe <hartmut@php.net>
2016-11-12 17:32:23 +0100

    extended test script to generate all output format for all styles

--- test/all-styles.sh
+++ test/all-styles.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
-rm -f *.png *.log *.err *.csv
+rm -f test*
 
 for style in $(grep name: /home/maposmatic/.ocitysmap.conf | grep -v '#' | grep -v 'Overlay' | sed -e 's/name://g')
 do
   echo "Testing '$style' style"
-  sudo -u maposmatic /home/maposmatic/ocitysmap/render.py --config=/home/maposmatic/.ocitysmap.conf --bounding-box=52.0268,8.5274 52.0329,8.5408 --title="Umgebungsplan Test" --format=png --prefix=test-$style  --language=de_DE.utf8 --layout=plain --orientation=landscape --paper-format=A1 --style=$style > test-$style.log 2> test-$style.err
+  for format in png pdf svgz
+  do
+    echo "... $format"
+    echo "sudo -u maposmatic /home/maposmatic/ocitysmap/render.py --config=/home/maposmatic/.ocitysmap.conf --bounding-box=52.0268,8.5274 52.0329,8.5408 --title='Umgebungsplan Test' --format=$format --prefix=test-$style --language=de_DE.utf8 --layout=plain --orientation=landscape --paper-format=A1 --style=$style" > test-$style-$format.sh
+    . test-$style-$format.sh > test-$style-$format.log 2> test-$style-$format.err
+  done
 done
