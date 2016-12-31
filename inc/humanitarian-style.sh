529a581ae351b2f786d9ccc2cd4f2717edf9827a
Hartmut Holzgraefe <hartmut@php.net>
2016-12-31 19:07:26 +0100

    disabled hillshading and contours for testing, simplified build as we now have carto version that understands yaml directly

--- inc/humanitarian-style.sh
+++ inc/humanitarian-style.sh
@@ -6,25 +6,24 @@
 
 cd /home/maposmatic/styles
 
-git clone https://github.com/hotosm/HDM-CartoCSS.git
+git clone https://github.com/hholzgra/HDM-CartoCSS.git
 
 cd HDM-CartoCSS
 
-cp -r ../openstreetmap-carto/scripts/ .
+git checkout nohillshade
 
 sed -e's|/ybon/Data/geo/shp/|/maposmatic/styles/openstreetmap-carto/data/|g' \
     -e's|/ybon/Code/maps/hdm/|/maposmatic/styles/HDM-CartoCSS/|g' \
     -e's|dbname: hdm|dbname: gis|g' \
     -e's|user: osm|user: maposmatic|g' \
-    < project.yml > project.yaml
-
-./scripts/yaml2mml.py
+    < project.yml > project.mml
 
 carto project.mml > osm.xml
 
-cd DEM
-mkdir -p data
-./fetch.sh 38,1,40,3 # TODO - this only fetches a small part of Germany
-./hillshade.sh
-./hillshade_to_vrt.sh
-./merge_contour.sh
+
+# cd DEM
+# mkdir -p data
+# ./fetch.sh 38,1,40,3 # TODO - this only fetches a small part of Germany
+# ./hillshade.sh
+# ./hillshade_to_vrt.sh
+# ./merge_contour.sh
