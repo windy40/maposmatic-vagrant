79629a8448efcc699505c3de17d1df6293ed42c5
Hartmut Holzgraefe <hartmut@php.net>
2017-01-01 14:49:35 +0100

    silence carto warnings about deprecated name attribute

--- inc/hikebike-style.sh
+++ inc/hikebike-style.sh
@@ -6,10 +6,17 @@
 
 cd /home/maposmatic/styles
 
+# there doesn't seem to be a public repository for this style? :(
 wget -O - https://dl.dropboxusercontent.com/u/279938/hikebikemap-carto-0.9.tbz | tar -xjf -
 cd hikebikemap-carto-0.9/ 
+
+# we can share the shapefiles with the OSM Carto style
 rm -rf data
 ln -s ../openstreetmap-carto/data/ .
-carto project.mml > osm.xml
 
+# remove deprecated name attributes from layers to silence carto warnings
+sed -i '/name:/d' project.mml
+
+# convert CartoCSS to Mapnil XML
+carto project.mml > osm.xml
 
