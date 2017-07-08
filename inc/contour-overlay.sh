866ed1bddd9896e58c8b714d6330ef3bc8f2a96d
Hartmut Holzgraefe <hartmut@php.net>
2017-07-09 00:42:00 +0200

    install script for contour overlay style

---
+++ inc/contour-overlay.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+cd /home/maposmatic/styles
+
+mkdir contour-overlay
+cd contour-overlay
+cp /vagrant/files/contour.xml .
+
+cat <<EOF >> /home/maposmatic/ocitysmap/ocitysmap.styledefs
+[contour_overlay]
+name: ContourOverlay
+description: Countour lines at 10m resolution
+path: /home/maposmatic/styles/contour-overlay/contour.xml
+
+EOF
+
+echo "  contour_overlay," >> /home/maposmatic/ocitysmap/ocitysmap.overlays
+
+
