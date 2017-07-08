4b7f681114feb5a3a9f9fba05ae5ca8e1da0f52f
Hartmut Holzgraefe <hartmut@php.net>
2017-07-08 22:45:47 +0200

    added space station style and fixed a few calls to other style setup scripts

---
+++ inc/spacestation-style.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+cd /home/maposmatic/styles
+
+git clone https://github.com/hholzgra/mapbox-studio-space-station.tm2.git
+cd mapbox-studio-space-station.tm2
+git checkout dev-osm2pgsql
+
+cat <<EOF >> /home/maposmatic/ocitysmap/ocitysmap.styledefs
+[spacestation]
+name: Spacestation
+description: Space station style - originally by Eleanor Lutz of Mabpox 
+path: /home/maposmatic/styles/mapbox-studio-space-station.tm2/osm.xml
+
+EOF
+
+echo "  spacestation," >> /home/maposmatic/ocitysmap/ocitysmap.styles
+
