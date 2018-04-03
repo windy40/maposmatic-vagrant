972bdb56a6091cf6995851883f78638ed96fa0da
Hartmut Holzgraefe <hartmut@php.net>
2018-04-03 19:42:33 +0200

    use centralized shapefiles

--- inc/openriverboat-style.sh
+++ inc/openriverboat-style.sh
@@ -8,27 +8,10 @@ cd OpenRiverboatMap/openriverboatmap
 mkdir data
 cd data
 
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_land.zip
-unzip ne_10m_land.zip
-for a in ne_10m_land.*
+for a in shoreline_300 ne_10m_land coastlines-split-3857
 do
-  b=$(echo $a | sed -e 's/^ne_10m_/10m-/g')
-  ln -s $a $b
+	ln -s /home/maposmatic/shapefiles/$a .
 done
-rm ne_10m_land.zip
-
-wget http://data.openstreetmapdata.com/coastlines-split-3857.zip
-unzip coastlines-split-3857.zip
-for a in coastlines-split-3857/lines.*
-do 
-  b=$(echo $a | sed -e 's/lines/coastline-good/g')
-  ln -s $a $b
-done
-rm coastlines-split-3857.zip
-
-wget http://tile.openstreetmap.org/shoreline_300.tar.bz2
-tar -xvf shoreline_300.tar.bz2
-rm shoreline_300.tar.bz2
 
 cd ..
 
@@ -36,8 +19,8 @@ sed -e 's/"dbname": "osm"/"dbname": "gis"/g' \
     -e 's/"host": "localhost"/"host": "gis-db"/g' \
     -e 's/"user": "osm"/"user": "maposmatic"/g' \
     -e 's/"password": "osm"/"password": "secret"/g' \
-    -e 's|http://tilemill-data.s3.amazonaws.com/osm/shoreline_300.zip|data/shoreline_300.shp|' \
-    -e 's|http://mapbox-geodata.s3.amazonaws.com/natural-earth-1.3.0/physical/10m-land.zip|data/10m-land.shp|' \
+    -e 's|http://tilemill-data.s3.amazonaws.com/osm/shoreline_300.zip|data/shoreline_300/shoreline_300.shp|' \
+    -e 's|http://mapbox-geodata.s3.amazonaws.com/natural-earth-1.3.0/physical/10m-land.zip|data/ne_10m_land/ne_10m_land.shp|' \
     -e 's|http://tilemill-data.s3.amazonaws.com/osm/coastline-good.zip|data/coastlines-split-3857/lines.shp|' \
     -e '/"name":/d' \
     -i openriverboatmap.mml
