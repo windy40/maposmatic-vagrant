4eb1fe0bf079a83a0a050053d4962bef5c1908a5
Hartmut Holzgraefe <hartmut@php.net>
2018-04-16 08:58:16 +0200

    use local shared shapefiles

--- inc/humanitarian-style.sh
+++ inc/humanitarian-style.sh
@@ -12,13 +12,20 @@ cd HDM-CartoCSS
 
 git checkout nohillshade
 
-sed -e's|/ybon/Data/geo/shp/|/maposmatic/styles/openstreetmap-carto/data/|g' \
+sed -e's|/ybon/Data/geo/shp/|/maposmatic/shapefiles/|g' \
     -e's|/ybon/Code/maps/hdm/|/maposmatic/styles/HDM-CartoCSS/|g' \
-    -e's|dbname: hdm|dbname: gis|g' \
-    -e's|user: osm|user: maposmatic|g' \
+    -e's|http://data.openstreetmapdata.com/land-polygons-split-3857.zip|/home/maposmatic/shapefiles/land-polygons-split-3857/land_polygons.shp|g' \
+    -e's|http://data.openstreetmapdata.com/simplified-land-polygons-complete-3857.zip|/home/maposmatic/shapefiles/simplified-land-polygons-complete-3857/simplified_land_polygons.shp|g' \
+    -e"s|layer \~|tags->'layer' \~|g" \
+    -e's|dbname: osm|dbname: gis|g' \
+    -e'/host:/d' \
+    -e'/user:/d' \
+    -e'/password:/d' \
     < project.yml > project.mml
 
 carto project.mml > osm.xml
+
+# -e's|user: hot|user: maposmatic|g' \
 
 
 # cd DEM
