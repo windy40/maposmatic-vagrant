c3f5b3d9a1d7713eb5d2222b0a2db3b7a50c6740
Hartmut Holzgraefe <hartmut@php.net>
2018-04-16 08:39:58 +0200

    use shared shapefiles

--- inc/veloroad-style.sh
+++ inc/veloroad-style.sh
@@ -6,7 +6,17 @@ git clone https://github.com/hholzgra/veloroad.git
 cd veloroad
 git checkout hstore
 
-./get-shapefiles.sh
+mkdir -p data
+ln -s /home/maposmatic/shapefiles/water-polygons-split-3857 data
+ln -s /home/maposmatic/shapefiles/gmted25 data/gmted
+
+curl -z data/ptsans.zip -L -o data/ptsans.zip https://www.fontsquirrel.com/fonts/download/pt-sans
+unzip -o data/ptsans.zip -d data/ptsans
+
+curl -z data/ptsans/DroidSansFallback.ttf -L -o data/ptsans/DroidSansFallback.ttf https://github.com/android/platform_frameworks_base/raw/master/data/fonts/DroidSansFallback.ttf
+
+
+
 
 sed '/"name":/d' < project.mml > osm.mml
 carto osm.mml > veloroad.xml
