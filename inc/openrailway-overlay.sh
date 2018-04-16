ec449336b7c25fa3084cb513a026be24d1a3631c
Hartmut Holzgraefe <hartmut@php.net>
2018-04-16 08:59:18 +0200

    with the hstore-only + views schema there's no need for an extra view here anymore

--- inc/openrailway-overlay.sh
+++ inc/openrailway-overlay.sh
@@ -6,18 +6,7 @@ git clone https://github.com/Nakaner/OpenRailwayMap-webmap-styles
 
 cd OpenRailwayMap-webmap-styles
 
-carto -a $(mapnik-config -v) project.mml | sed -e 's/planet_osm_line/railmap_line/g' -e 's/comp-op="screen"//g' >  railmap.xml 2>/dev/null
-
-sudo -u maposmatic psql gis <<EOF
-DROP VIEW railmap_line;
-
-CREATE VIEW railmap_line 
-  AS SELECT *
-          , tags->'usage'    AS usage
-	  , tags->'razed'    AS razed 
-	  , tags->'proposed' AS proposed 
-       FROM planet_osm_line;
-EOF
+carto -a $(mapnik-config -v) project.mml | sed -e 's/comp-op="screen"//g' >  railmap.xml 2>/dev/null
 
 cat <<EOF >> /home/maposmatic/ocitysmap/ocitysmap.styledefs
 [rail_overlay]
