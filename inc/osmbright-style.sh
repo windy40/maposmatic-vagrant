b9e40ed09c4de7fe5a4e695c70199b60aca9e17f
Hartmut Holzgraefe <hartmut@php.net>
2018-04-21 10:38:56 +0200

    make OSM-Bright use shared shapefiles

--- inc/osmbright-style.sh
+++ inc/osmbright-style.sh
@@ -6,32 +6,9 @@ git clone https://github.com/mapbox/osm-bright.git
 
 cd osm-bright
 
-mkdir shp
+ln -s /home/maposmatic/shapefiles shp
 
-cd shp
-
-wget http://data.openstreetmapdata.com/simplified-land-polygons-complete-3857.zip
-unzip simplified-land-polygons-complete-3857.zip
-cd simplified-land-polygons-complete-3857
-shapeindex simplified_land_polygons.shp
-cd ..
-
-wget http://data.openstreetmapdata.com/land-polygons-split-3857.zip
-unzip land-polygons-split-3857.zip
-cd land-polygons-split-3857
-shapeindex land_polygons.shp
-cd ..
-
-mkdir 10m-populated-places-simple
-cd 10m-populated-places-simple
-wget http://mapbox-geodata.s3.amazonaws.com/natural-earth-1.4.0/cultural/10m-populated-places-simple.zip
-unzip 10m-populated-places-simple.zip
-shapeindex 10m-populated-places-simple.shp
-cd ..
-
-cd ..
-
-cp /vagrant/files/configure.py .
+cp /vagrant/files/osmbright-configure.py .
 
 ./make.py
 
