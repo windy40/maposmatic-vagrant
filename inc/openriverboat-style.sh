32756ae2bb0a99e75828b94b02c2600e84905dfb
Hartmut Holzgraefe <hartmut@php.net>
2018-07-25 15:48:15 +0200

    use shared shapefiles, hstore tags array for checking layers

--- inc/openriverboat-style.sh
+++ inc/openriverboat-style.sh
@@ -5,25 +5,18 @@ cd /home/maposmatic/styles
 git clone https://github.com/yohanboniface/OpenRiverboatMap.git
 cd OpenRiverboatMap/openriverboatmap
 
-mkdir data
-cd data
-
-for a in shoreline_300 ne_10m_land coastlines-split-3857
-do
-	ln -s /home/maposmatic/shapefiles/$a .
-done
-
-cd ..
+ln -s /home/maposmatic/shapefiles data
 
 sed -e 's/"dbname": "osm"/"dbname": "gis"/g' \
     -e 's/"host": "localhost"/"host": "gis-db"/g' \
     -e 's/"user": "osm"/"user": "maposmatic"/g' \
     -e 's/"password": "osm"/"password": "secret"/g' \
+    -e "s/layer \~/tags->'layer' \~/g" \
     -e 's|http://tilemill-data.s3.amazonaws.com/osm/shoreline_300.zip|data/shoreline_300/shoreline_300.shp|' \
     -e 's|http://mapbox-geodata.s3.amazonaws.com/natural-earth-1.3.0/physical/10m-land.zip|data/ne_10m_land/ne_10m_land.shp|' \
     -e 's|http://tilemill-data.s3.amazonaws.com/osm/coastline-good.zip|data/coastlines-split-3857/lines.shp|' \
     -e '/"name":/d' \
-    -i openriverboatmap.mml
+    < openriverboatmap.mml > processed.mml
 
 carto $(mapnik-config -v) processed.mml > openriverboatmap.xml
 
