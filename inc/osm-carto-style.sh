851c3cffbbf0b8912aff7a13d09032fc18bf64ee
Hartmut Holzgraefe <hartmut@php.net>
2018-11-23 16:53:59 +0100

    update to lagest CartoOSM release

--- inc/osm-carto-style.sh
+++ inc/osm-carto-style.sh
@@ -7,7 +7,7 @@
 cd /home/maposmatic/styles
 git clone https://github.com/gravitystorm/openstreetmap-carto.git
 cd openstreetmap-carto
-git checkout v4.13.0
+git checkout v4.17.0
 
 ln -s /home/maposmatic/shapefiles data
 
