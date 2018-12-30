850a24acd7fc4439bb77e4017c42c34149266e82
Hartmut Holzgraefe <hartmut@php.net>
2018-12-30 18:41:42 +0100

    fix database settings for Belgian style

--- inc/belgian-style.sh
+++ inc/belgian-style.sh
@@ -10,7 +10,12 @@ cd openstreetmap-carto-be
 
 ln -s /home/maposmatic/shapefiles data
 
-sed '/\sname:/d' < project.mml > osm.mml
+sed -e '/\sname:/d' \
+    -e 's/host:.*/host: "gis-db"/' \
+    -e 's/user:.*/user: "maposmatic"\n    password: "secret"/' \
+    -e 's/dbname:.*/dbname: "gis"/' \
+< project.mml > osm.mml
+
 carto -q -a $(mapnik-config -v) --quiet osm.mml > osm.xml
 php /vagrant/files/postprocess-style.php osm.xml
 
