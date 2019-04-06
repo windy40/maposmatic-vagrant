a24ed18ec53f3868db77c3087ff4f5741872b782
Hartmut Holzgraefe <hartmut@php.net>
2019-04-07 01:36:02 +0200

    replace coastline-good shape with land-polygons-split, not coastline-split

--- inc/blossom-style.sh
+++ inc/blossom-style.sh
@@ -9,10 +9,11 @@ cd blossom
 sed '/"name":/d' < project.mml > osm.mml
 sed -ie 's/"dbname": "osm"/"dbname": "gis"/g' osm.mml
 sed -ie 's/http.*10m-land.zip"/\/home\/maposmatic\/shapefiles\/ne_10m_land\/ne_10m_land.shp", "type": "shape"/' osm.mml
-sed -ie 's/http.*coastline-good.zip"/\/home\/maposmatic\/shapefiles\/coastlines-split-3857\/lines.shp", "type": "shape"/' osm.mml
+sed -ie 's/http.*coastline-good.zip"/\/home\/maposmatic\/shapefiles\/land-polygons-split-3857\/land_polygons.shp", "type": "shape"/' osm.mml
 sed -ie 's/http.*shoreline_300.zip"/\/home\/maposmatic\/shapefiles\/shoreline_300\/shoreline_300.shp", "type": "shape"/' osm.mml
 sed -ie 's/http.*10m-populated-places-simple.zip"/\/home\/maposmatic\/shapefiles\/ne_10m_populated_places_simple\/ne_10m_populated_places_simple.shp", "type": "shape"/' osm.mml
 sed -ie 's/"srs": ""/"srs": "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0.0 +k=1.0 +units=m +nadgrids=@null +wktext +no_defs +over"/g' osm.mml
+
 carto -a $(mapnik-config -v) -q osm.mml  > osm.xml
 php /vagrant/files/postprocess-style.php osm.xml
 
--- inc/openriverboat-style.sh
+++ inc/openriverboat-style.sh
@@ -14,7 +14,7 @@ sed -e 's/"dbname": "osm"/"dbname": "gis"/g' \
     -e "s/layer \~/tags->'layer' \~/g" \
     -e 's|http://tilemill-data.s3.amazonaws.com/osm/shoreline_300.zip|data/shoreline_300/shoreline_300.shp|' \
     -e 's|http://mapbox-geodata.s3.amazonaws.com/natural-earth-1.3.0/physical/10m-land.zip|data/ne_10m_land/ne_10m_land.shp|' \
-    -e 's|http://tilemill-data.s3.amazonaws.com/osm/coastline-good.zip|data/coastlines-split-3857/lines.shp|' \
+    -e 's|http://tilemill-data.s3.amazonaws.com/osm/coastline-good.zip|data/land-polygons-split-3857/land_polygons.shp|' \
     -e '/"name":/d' \
     < openriverboatmap.mml > processed.mml
 
