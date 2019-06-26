48dbf019de67ac78207898f5e7abdba1e5656a81
Hartmut Holzgraefe <hartmut@php.net>
2019-06-26 22:38:08 +0200

    add paper size definitions to renderer config file

--- inc/ocitysmap-conf.sh
+++ inc/ocitysmap-conf.sh
@@ -7,6 +7,15 @@ user=maposmatic
 password=secret
 dbname=gis
 
+[paper_sizes]
+Din A4: 210x297
+Din A3: 297x420
+Din A2: 420x594
+Din A1: 594x841
+Din A0: 841x1189
+2x A0: 1189x1682
+US letter: 216x279
+
 [rendering]
 available_stylesheets:
 EOF
