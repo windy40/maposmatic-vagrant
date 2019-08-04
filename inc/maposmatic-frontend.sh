2a1b6f7c8f01ff163f21ac93f02eb9f79d34b961
Hartmut Holzgraefe <hartmut@php.net>
2019-08-04 13:16:04 +0200

    use same git branch as the public site

--- inc/maposmatic-frontend.sh
+++ inc/maposmatic-frontend.sh
@@ -8,7 +8,7 @@
 cd /home/maposmatic
 git clone https://github.com/hholzgra/maposmatic.git
 cd maposmatic
-git checkout site-osm-baustelle-newform
+git checkout site-osm-baustelle
 
 
 # install dependencies
