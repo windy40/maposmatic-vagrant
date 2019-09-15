415fae9983a94e81da348418f98b275826d1b04b
Hartmut Holzgraefe <hartmut@php.net>
2019-09-15 12:47:30 +0200

    create hillshading and contours for actual import area
    previously only a fixed set of SRTM 90m zone files were processed

--- inc/elevation-model.sh
+++ inc/elevation-model.sh
@@ -2,34 +2,58 @@
 
 cd /home/maposmatic
 
+
+
+# read SRTM 90m zone name -> area mapping table
+echo "Importing SRTM zone database"
+sudo -u maposmatic psql gis < /vagrant/files/srtm_zones.sql > /dev/null
+
 mkdir -p elevation-data
 cd elevation-data
 
+echo "Downloading SRTM arcive files"
 
-echo "strm hillshading for PisteMap"
+mkdir -p srtm-data
+cd srtm-data
+
+mkdir -p $CACHEDIR/srtm-data
+
+# extract bounding box data in bash array foratm
+bbox=$(osmium fileinfo --no-progress --extended --get data.bbox $OSM_EXTRACT | sed -e's/,/ /g')
+
+# create actaul bounding box bash array
+eval b=$bbox
+
+# create bounding box polygon WKT string
+polygon="POLYGON((${b[1]} ${b[0]}, ${b[1]} ${b[2]}, ${b[3]} ${b[2]}, ${b[3]} ${b[0]}, ${b[1]} ${b[0]}))"
+
+# now download all zones the import bounding box overlaps with
+for zone in $(psql gis --tuples-only --command="select zone from srtm_zones where ST_INTERSECTS(way, ST_GeomFromText('$polygon', 4326))")
+do
+    CACHEFILE=$CACHEDIR/srtm-data/$zone.zip
+    SRTM_URL=http://viewfinderpanoramas.org/dem3/$zone.zip
+    if ! test -f $CACHEFILE
+    then
+	echo "  downloading zone $zone"
+	wget -q -O $CACHEFILE $SRTM_URL
+    fi
+    unzip -q $CACHEFILE
+done
+
+cd ..
+
+
+
+
+echo "SRTM hillshading for PisteMap"
 
 mkdir -p srtm
 cd srtm
 
-# see http://www.viewfinderpanoramas.org/Coverage%20map%20viewfinderpanoramas_org3.htm for letters/numbers
-
-for x in N # L M N
-do
-  for y in 32 # 31 32 33
-  do
-    wget http://viewfinderpanoramas.org/dem3/$x$y.zip
-  done
-done
-  
-for zip in *.zip
-do
-  unzip $zip
-done
-
-for file in $(ls **/*.hgt | sort)
+for file in $(find /home/maposmatic/elevation-data/srtm-data/ -name "*.hgt" | sort)
 do
     base=$(basename $file .hgt)
-
+    echo "  processing $base"
     gdal_translate -q -of GTiff -co "TILED=YES" -a_srs "+proj=latlong" $file ${base}_adapted.tif
 
     gdalwarp -q -multi -of GTiff -co "TILED=YES" -srcnodata 32767 -t_srs "+proj=merc +ellps=sphere +R=6378137 +a=6378137 +units=m" -rcs -order 3 -tr 30 30 -multi ${base}_adapted.tif ${base}_warped.tif
@@ -37,26 +61,32 @@ do
     gdaldem hillshade -q ${base}_warped.tif ${base}_hillshade.tif
 done
 
-
 cd ..
+
+
+
 
 echo "DEM data for OpenTopoMap"
 mkdir -p dem
 cd dem
 
+# file taken from OpenTopoMap repository, which may not be installed at this point yet
 cp /vagrant/files/relief_color_text_file.txt .
 
-for a in $(find /home/maposmatic/elevation-data/srtm -name "*.hgt")
+# fill empty spaces
+for file in $(find /home/maposmatic/elevation-data/srtm-data -name "*.hgt" | sort)
 do
-  echo $(basename $a)
-  gdal_fillnodata.py -q $a $(basename $a).tif
+  echo "  processing "$(basename $file .hgt)
+  gdal_fillnodata.py -q $file $(basename $file).tif
 done
 
-gdal_merge.py -n 32767 -co BIGTIFF=YES -co TILED=YES -co COMPRESS=LZW -co PREDICTOR=2 -o raw.tif *.hgt.tif
-ln -s raw.tif dem-srtm.tiff
+# merge all elevation data into one single large tiled file
+gdal_merge.py -n 32767 -co BIGTIFF=YES -co TILED=YES -co COMPRESS=LZW -co PREDICTOR=2 -o raw.tif *.hgt.tif -q
 
+ln -s raw.tif dem-srtm.tiff
 ln -s raw.tif dem_srtm.tiff
 
+# convert to google mercator projection
 interpolation=cubicspline
 for r in 90 500 1000 5000
 do
@@ -64,19 +94,23 @@ do
   interpolation=bilinear
 done
 
+# create colored reliefs for low zoom levels
 gdaldem color-relief -co COMPRESS=LZW -co PREDICTOR=2 -alpha warp-5000.tif relief_color_text_file.txt relief-5000.tif -q
-gdaldem color-relief -co COMPRESS=LZW -co PREDICTOR=2 -alpha warp-500.tif relief_color_text_file.txt relief-500.tif -q
+gdaldem color-relief -co COMPRESS=LZW -co PREDICTOR=2 -alpha warp-500.tif  relief_color_text_file.txt relief-500.tif -q
 
+# create hillshading 
 gdaldem hillshade -z 7 -compute_edges -co COMPRESS=JPEG warp-5000.tif hillshade-5000.tif -q
 gdaldem hillshade -z 7 -compute_edges -co BIGTIFF=YES -co TILED=YES -co COMPRESS=JPEG warp-1000.tif hillshade-1000.tif -q
 gdaldem hillshade -z 4 -compute_edges -co BIGTIFF=YES -co TILED=YES -co COMPRESS=JPEG warp-500.tif hillshade-500.tif -q
-gdaldem hillshade -z 2 -co compress=lzw -co predictor=2 -co bigtiff=yes -compute_edges warp-90.tif hillshade-90.tif && gdal_translate -co compress=JPEG -co bigtiff=yes -co tiled=yes hillshade-90.tif hillshade-90-jpeg.tif -q
+gdaldem hillshade -z 7 -combined -compute_edges -co compress=lzw -co predictor=2 -co bigtiff=yes warp-90.tif hillshade-90.tif -q
+# TODO: not used? gdal_translate -co compress=JPEG -co bigtiff=yes -co tiled=yes hillshade-90.tif hillshade-90-jpeg.tif -q
 
-echo "contours schema"
+# set up countours database and table schema
 sudo -u maposmatic psql gis < /vagrant/files/contours_schema.sql >/dev/null
 
-echo "countours data"
-sudo -u maposmatic psql contours < /vagrant/files/contours_53-8.sql >/dev/null
+# create contours shapefile and imports its data into the database
+gdal_contour -i 10 -a ele warp-90.tif . -q
+shp2pgsql -a -g way -s contour.shp contours | psql contours
 
 cd ..
 
