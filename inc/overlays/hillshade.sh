28f6bc23eb1158cc7343f5645f0fa8b146142df0
Hartmut Holzgraefe <hartmut@php.net>
2019-09-16 20:56:42 +0200

    added hillshade overlay

---
+++ inc/overlays/hillshade.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+mkdir -p /home/maposmatic/styles/hillshade-overlay
+cd /home/maposmatic/styles/hillshade-overlay
+
+cp /vagrant/files/hillshade.xml .
+ln -s /home/maposmatic/elevation-data/dem .
