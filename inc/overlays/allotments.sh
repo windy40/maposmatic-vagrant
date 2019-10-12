0839ea4e314ac684db231f5061f0456997cf12d4
Hartmut Holzgraefe <hartmut@php.net>
2019-10-12 16:50:44 +0200

    added Mapnik-allotments overlay style

---
+++ inc/overlays/allotments.sh
@@ -0,0 +1,10 @@
+#----------------------------------------------------
+#
+# "Allotments" overlay
+#
+#----------------------------------------------------
+
+cd /home/maposmatic/styles
+
+git clone --quiet https://github.com/hholzgra/Mapnik-allotments.git
+
