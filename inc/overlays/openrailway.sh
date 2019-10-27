f65d5fe47aa5fcdfe0b57ea6cef6be73e4e2a36c
Hartmut Holzgraefe <hartmut@php.net>
2019-10-27 20:43:06 +0100

    style version update requires additional SQL import

--- inc/overlays/openrailway.sh
+++ inc/overlays/openrailway.sh
@@ -8,6 +8,7 @@ cd OpenRailwayMap-webmap-styles
 
 sudo -u maposmatic psql gis < sql/osm_carto_views.sql
 sudo -u maposmatic psql gis < sql/get_station_importance.sql
+sudo -u maposmatic psql gis < sql/functions.sql
 
 carto -a $(mapnik-config -v) --quiet project.mml  > railmap.xml
 php /vagrant/files/tools/postprocess-style.php railmap.xml
