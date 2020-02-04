f61a3f73f29f9f1a07880827e737d67c02723706
Hartmut Holzgraefe <hartmut@php.net>
2020-02-04 17:04:08 +0100

    found no better way to silence pandoc progress output to stderr

--- inc/mapnik-german-l10n.sh
+++ inc/mapnik-german-l10n.sh
@@ -20,6 +20,11 @@ cd tools
 
 git clone --quiet https://github.com/giggls/mapnik-german-l10n.git
 cd mapnik-german-l10n
+for target in README INSTALL TODO
+do
+	make $target 2>/dev/null
+	make $target.html 2>/dev/null
+done
 make install
 sudo --user=maposmatic psql --dbname=gis --command="CREATE EXTENSION osml10n"
 
