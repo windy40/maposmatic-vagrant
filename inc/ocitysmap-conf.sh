c0e989609a0ae8f1b4e6972080b4f24814c30599
Hartmut Holzgraefe <hartmut@php.net>
2020-03-12 11:23:25 +0000

    use '=' for ini file value assignments, not ':'

--- inc/ocitysmap-conf.sh
+++ inc/ocitysmap-conf.sh
@@ -10,25 +10,25 @@ password=secret
 dbname=gis
 
 [paper_sizes]
-Din A4: 210x297
-Din A3: 297x420
-Din A2: 420x594
-Din A1: 594x841
-Din A0: 841x1189
-2x A0: 1189x1682
-US letter: 216x279
+Din A4= 210x297
+Din A3= 297x420
+Din A2= 420x594
+Din A1= 594x841
+Din A0= 841x1189
+2x A0= 1189x1682
+US letter= 216x279
 
 [multipage_paper_sizes]
-Din A4: 210x297
-US letter: 216x279
+Din A4= 210x297
+US letter= 216x279
 
 [rendering]
 EOF
 
-echo -n "available_stylesheets: " >> $CONF
+echo -n "available_stylesheets= " >> $CONF
 grep --no-filename '\[.*\]' /vagrant/inc/styles/*.ini | sed -e 's/\[//g' -e 's/\]//g' | paste -sd "," >> $CONF
 
-echo -n "available_overlays: compass_rose,scalebar,osm_notes,qrcode,surveillance," >> $CONF
+echo -n "available_overlays= compass_rose,scalebar,osm_notes,qrcode,surveillance," >> $CONF
 grep --no-filename '\[.*\]' /vagrant/inc/overlays/*.ini | sed -e 's/\[//g' -e 's/\]//g' | paste -sd "," >> $CONF
 
 cat /vagrant/inc/styles/*.ini /vagrant/inc/overlays/*.ini >> $CONF
