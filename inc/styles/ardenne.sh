70c4a4dd4007938055a256ff534d8fe2e5eb325a
Hartmut Holzgraefe <hartmut@php.net>
2020-04-26 09:47:56 +0200

    typo fix in contour dir name

--- inc/styles/ardenne.sh
+++ inc/styles/ardenne.sh
@@ -13,7 +13,7 @@ cd downloads
 wget http://opendata.champs-libres.be/beautiful-contour-belgium.zip
 unzip beautiful-contour-belgium.zip
 cd beautiful-contour-belgium
-ogr2ogr -f "ESRI Shapefile" ../../contours beautiful-contour-belgium.gpkg
+ogr2ogr -f "ESRI Shapefile" ../../contour beautiful-contour-belgium.gpkg
 
 cd ../../contour/
 mmv 'beautiful-contour-belgium.*' 'beautiful_contour_belgium.#1'
