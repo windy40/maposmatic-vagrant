ab00e448668faa7c3dcad8de391d0eeaca590107
Hartmut Holzgraefe <hartmut@php.net>
2020-04-26 20:05:29 +0200

    silence LaTeX pgrogress output

--- inc/testing.sh
+++ inc/testing.sh
@@ -6,6 +6,5 @@ rm -f test-* thumbnails/test-*
 ./run-tests.sh
 
 php /vagrant/files/tools/all-styles-pdf.php > all-styles.tex
-pdflatex all-styles.tex
+pdflatex all-styles.tex > /dev/null
 
-# pdfjam --suffix nup --quiet --nup 5x5 --papersize '{594mm,841mm}' --outfile all-styles-poster.pdf test-base-*-pdf.pdf test-overlay-*-pdf.pdf
