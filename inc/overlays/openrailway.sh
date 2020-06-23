89091c942b14a653f2fdc2fb0ffef0d4a732109b
Hartmut Holzgraefe <hartmut@php.net>
2020-06-23 10:38:00 +0200

    adjustments for latest openrailwail style changes

--- inc/overlays/openrailway.sh
+++ inc/overlays/openrailway.sh
@@ -2,18 +2,19 @@
 
 cd /home/maposmatic/styles
 
-git clone --quiet https://github.com/Nakaner/OpenRailwayMap-webmap-styles
+git clone --quiet https://github.com/OpenRailwayMap/OpenRailwayMap-CartoCSS
 
-cd OpenRailwayMap-webmap-styles
+cd OpenRailwayMap-CartoCSS
 
 sudo -u maposmatic psql gis < sql/osm_carto_views.sql
-sudo -u maposmatic psql gis < sql/get_station_importance.sql
 sudo -u maposmatic psql gis < sql/functions.sql
+sudo -u maposmatic psql gis < sql/get_station_importance.sql
 
-carto -a $(mapnik-config -v) --quiet project.mml  > railmap.xml
-php /vagrant/files/tools/postprocess-style.php railmap.xml
-
-carto -a $(mapnik-config -v) --quiet maxspeed.mml  > maxspeed.xml
-php /vagrant/files/tools/postprocess-style.php maxspeed.xml
+for carto_style in *.mml
+do
+	mapnik_style=$(basename $carto_style .mml).xml
+	carto -a $(mapnik-config -v) --quiet $carto_style  > $mapnik_style
+        php /vagrant/files/tools/postprocess-style.php $mapnik_style
+done
 
 
