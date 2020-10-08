93b83858db0c6e1d3a391e6ff07823703dc0b308
Hartmut Holzgraefe <hartmut@php.net>
2020-10-08 20:34:19 +0000

    make new Belgian style work again

--- inc/styles/belgian.sh
+++ inc/styles/belgian.sh
@@ -10,6 +10,24 @@ cd openstreetmap-carto-be
 
 ln -s /home/maposmatic/shapefiles data
 
+git clone --quiet https://github.com/gravitystorm/openstreetmap-carto
+
+cd openstreetmap-carto
+git checkout --quiet v5.1.0
+rm -rf .git
+for file in $(find . -type f)
+do
+  dir=$(dirname $file)
+  mkdir -p ../$dir
+  if test ! -f ../$file
+  then
+    cp $file ../$file
+  fi
+done
+cd ..
+rm -rf openstreetmap-carto
+
+
 sed -e '/\sname:/d' \
     -e 's/host:.*/host: "gis-db"/' \
     -e 's/user:.*/user: "maposmatic"\n    password: "secret"/' \
