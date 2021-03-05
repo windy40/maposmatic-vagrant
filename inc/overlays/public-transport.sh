8878adc944308a1eea283430b258ed814d42d2f1
Hartmut Holzgraefe <hartmut@php.net>
2021-03-05 22:41:05 +0100

    switch to giggls openptmap repository
    initial cause: openptmap downtime, but it's also simpler
    this way anyway

--- inc/overlays/public-transport.sh
+++ inc/overlays/public-transport.sh
@@ -2,39 +2,13 @@
 
 cd /home/maposmatic/styles
 
-mkdir openptmap
+git clone https://github.com/giggls/openptmap
 
 cd openptmap
 
-ln -s /home/maposmatic/shapefiles/world_boundaries/ .
+sed -ie 's/>osm</>gis</g' datasource-settings.xml.inc
 
-wget -r -l 1 -nd -A \*.png -P symbols http://openptmap.org/f/symbols/
-
-wget http://openptmap.org/f/mapnik_pt.xml
-
-
-# use datasource entity instead of hard coded db parameters
-cp /vagrant/files/config-files/datasource-settings.xml.inc .
-
-sed -ie 's/<Parameter name="type">postgis<\/Parameter>/\&datasource-settings;/' mapnik_pt.xml
-
-sed -ie '/<Parameter name="host"/d' mapnik_pt.xml
-sed -ie '/<Parameter name="port"/d' mapnik_pt.xml
-sed -ie '/<Parameter name="user"/d' mapnik_pt.xml
-sed -ie '/<Parameter name="password/d' mapnik_pt.xml
-sed -ie '/<Parameter name="dbname/d' mapnik_pt.xml
-
-# make style work with standard osm2pgsql style 
-
-sed -ie "s/,bus/,tags->'bus' as bus/" mapnik_pt.xml
-sed -ie "s/ bus=/ tags->'bus'=/" mapnik_pt.xml
-
-sed -ie "s/,line/,tags->'line' as line/" mapnik_pt.xml
-sed -ie "s/ line=/ tags->'line'=/" mapnik_pt.xml
-
-sed -ie "s/,train/,tags->'train' as train/" mapnik_pt.xml
-sed -ie "s/ train=/ tags->'train'=/" mapnik_pt.xml
-
-sed -ie "s/ state=/ tags->'state'=/" mapnik_pt.xml
-sed -ie "s/state is null/tags->'state' is null/" mapnik_pt.xml
-
+for sql in views/*.sql
+do
+	psql gis < $sql
+done
