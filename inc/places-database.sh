0621f1ab6cf1f97c220f0f0b4f1866912c437336
Hartmut Holzgraefe <hartmut@php.net>
2021-05-02 12:02:00 +0200

    fix filename

--- inc/places-database.sh
+++ inc/places-database.sh
@@ -7,9 +7,9 @@ mkdir -p $DBDIR
 # create places table for replacing nominatim search
 if ! test -f $DBDIR/place.sql.gz
 then
-	wget https://www.osm-baustelle.de/downloads/place.sql.gz -O $DBDIR/place.sql.gz
+	wget https://www.osm-baustelle.de/downloads/places.sql.gz -O $DBDIR/places.sql.gz
 fi
-zcat $DBDIR/place.sql.gz | sudo -u maposmatic psql gis 
+zcat $DBDIR/places.sql.gz | sudo -u maposmatic psql gis 
 
 sudo -u maposmatic psql gis -c "CREATE INDEX place_osmid_idx ON public.place using btree(osm_id);"
 sudo -u maposmatic psql gis -c "CREATE INDEX place_lower ON public.place USING btree (lower((name)::text));"
