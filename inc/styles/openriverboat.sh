8c5e6f927419b766fce2b87d7ac37907dddde793
Hartmut Holzgraefe <hartmut@php.net>
2021-09-11 17:55:54 +0200

    change OpenRiverboatMap setup to reflect upstream changes

--- inc/styles/openriverboat.sh
+++ inc/styles/openriverboat.sh
@@ -3,20 +3,18 @@
 cd /home/maposmatic/styles
 
 git clone --quiet https://github.com/yohanboniface/OpenRiverboatMap.git
-cd OpenRiverboatMap/openriverboatmap
 
 ln -s /home/maposmatic/shapefiles data
 
-sed -e 's/"dbname": "osm"/"dbname": "gis"/g' \
-    -e 's/"host": "localhost"/"host": "gis-db"/g' \
-    -e 's/"user": "osm"/"user": "maposmatic"/g' \
-    -e 's/"password": "osm"/"password": "secret"/g' \
+sed -e 's/dbname: osm/dbname: gis/g' \
+    -e "s/host: ''/host: gis-db/g" \
+    -e 's/user: ybon/user: maposmatic/g' \
+    -e 's/password: null/password: secret/g' \
     -e "s/layer \~/tags->'layer' \~/g" \
-    -e 's|http://tilemill-data.s3.amazonaws.com/osm/shoreline_300.zip|data/shoreline_300/shoreline_300.shp|' \
-    -e 's|http://mapbox-geodata.s3.amazonaws.com/natural-earth-1.3.0/physical/10m-land.zip|data/ne_10m_land/ne_10m_land.shp|' \
-    -e 's|http://tilemill-data.s3.amazonaws.com/osm/coastline-good.zip|data/land-polygons-split-3857/land_polygons.shp|' \
+    -e 's|https://osmdata.openstreetmap.de/download/simplified-land-polygons-complete-3857.zip|data/simplified-land-polygons-complete-3857/simplified_land_polygons.shp|' \
+    -e 's|https://osmdata.openstreetmap.de/download/land-polygons-split-3857.zip|data/land-polygons-split-3857/land_polygons.shp|' \
     -e '/"name":/d' \
-    < openriverboatmap.mml > processed.mml
+    < project.yml > processed.mml
 
 carto -q -a $(mapnik-config -v) processed.mml > openriverboatmap.xml
 
