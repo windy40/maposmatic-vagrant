254318d95f1d72adebbaab2c855ab116cff14eb2
Hartmut Holzgraefe <hartmut@php.net>
2021-09-11 21:18:57 +0200

    adjustments for recent Oomap upstream changes

--- inc/styles/oomap.sh
+++ inc/styles/oomap.sh
@@ -2,13 +2,17 @@
 
 cd /home/maposmatic/styles
 
-git clone https://github.com/oobrien/oomap
+git clone https://github.com/hholzgra/oomap
 
-cd oomap/maptiler/
+cd oomap/
+
+git checkout maposmatic
+
+cd maptiler/
 
 cp /vagrant/files/styles/oomap/* styles/inc/
 
 ln -s /home/maposmatic/shapefiles .
 
-
-
+psql gis < /vagrant/inc/styles/oomap.sql
+shp2pgsql -g way /home/maposmatic/shapefiles/water-polygons-split-3857/water_polygons.shp public.water | psql gis > /dev/null
