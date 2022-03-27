b84a19b006600ca3cb9c15bc977d2c53b77b4c57
Hartmut Holzgraefe <hartmut@php.net>
2022-03-27 20:14:49 +0200

    fix apache envvars file path for configuration modifications

--- inc/apache-global-config.sh
+++ inc/apache-global-config.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sed -i -e 's/export LANG=C/#export LANG=C/' -e's/#. \/etc\/default\/locale/. \/etc\/default\/locale/' /etc/apache/envvars
+sed -i -e 's/export LANG=C/#export LANG=C/' -e's/#. \/etc\/default\/locale/. \/etc\/default\/locale/' /etc/apache2/envvars
