55ea41b25d67acfa4ab47407e0f502b6316cedb6
Hartmut Holzgraefe <hartmut@php.net>
2022-07-05 15:41:17 +0200

    MS core fonts are now installed by their own script

--- inc/install-packages/fonts.sh
+++ inc/install-packages/fonts.sh
@@ -4,6 +4,3 @@
 banner "extra fonts"
 (cd $INCDIR/fonts; for script in *.sh; do basename $script ".sh"; ( . $script ); done )
 
-echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
-
-    ttf-mscorefonts-installer \
