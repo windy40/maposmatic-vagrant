8b5b6231c9cb546fad56c5e09fd1030cfad84a18
Hartmut Holzgraefe <hartmut@php.net>
2022-09-01 16:00:21 +0200

    add Health Overlay

---
+++ inc/overlays/health.sh
@@ -0,0 +1,16 @@
+#----------------------------------------------------
+#
+# Fire/Emergency overlay
+#
+# Original on https://github.com/rweait/Mapnik-golf-overlay
+#
+#----------------------------------------------------
+
+cd /home/maposmatic/styles
+
+git clone --quiet https://github.com/hholzgra/Mapnik-health-overlay.git
+
+cd Mapnik-health-overlay
+# TODO add any extra tasks here
+cd ..
+
