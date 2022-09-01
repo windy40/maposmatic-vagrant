6cdeb0b87eab368c7a72fc0450bdac5939f070d8
Hartmut Holzgraefe <hartmut@php.net>
2022-09-01 16:05:37 +0200

    add health overlay

---
+++ inc/styles/health.sh
@@ -0,0 +1,16 @@
+#----------------------------------------------------
+#
+# Health Overlay style
+#
+# Original on https://github.com/rweait/Mapnik-health-overlay
+#
+#----------------------------------------------------
+
+cd /home/maposmatic/styles
+
+git clone --quiet https://github.com/hholzgra/Mapnik-health-overlay.git
+
+cd Mapnik-health-overlay
+# TODO add any extra tasks here
+cd ..
+
