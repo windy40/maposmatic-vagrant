ca887f8a01e115cdd9d342eafaac55956c119fdb
Hartmut Holzgraefe <hartmut@php.net>
2022-09-18 13:19:56 +0200

    quick/dirty baumkarte preprocessing fix for now

--- inc/styles/zz-baumkarte.sh
+++ inc/styles/zz-baumkarte.sh
@@ -5,7 +5,12 @@ cd $STYLEDIR
 git clone --quiet https://github.com/rudzick/Mymapnik.git baumkarte
 cd baumkarte
 
+rm -f /usr/local/bin/carto
 ln /usr/bin/carto /usr/local/bin/carto
+
+cp Baumsorten_Erweiterung/my_mms-files.mml Baumsorten_Erweiterung/my_mms-files_cwd.mml
+
+( cd ../openstreetmap-carto/; ln -s ../baumkarte/Baumsorten_Erweiterung . )
 
 ./mymapnik.bash /home/maposmatic/styles/openstreetmap-carto
 
