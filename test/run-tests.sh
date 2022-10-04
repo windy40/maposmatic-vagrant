4f696fdede87330980271d3fb85d1793ef64a103
Hartmut Holzgraefe <hartmut@php.net>
2022-10-05 00:45:00 +0200

    generate layout previews in multiple resolutions

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -44,32 +44,45 @@ else
 
   ## render
 
+  # TODO move preview img generation to own script,
+  #      enroll different sizes in loop
+  #      import BBOX and PAPER from common include script
+  
   echo -n "Plain page layout preview ..."
+  base="layout-plain"
   echo "ocitysmap --config=/home/maposmatic/.ocitysmap.conf --bounding-box=$BBOX --title='Plain' --format=png --prefix=layout-plain --language=de_DE.utf8 --layout=plain --orientation=landscape --paper-format='$PAPER' --style=CartoOSM" > $base.sh
   chmod a+x $base.sh
   /usr/bin/time -q -f "%E" -o $base.time ./$base.sh > $base.log 2> $base.err
-  convert -resize 500x355 layout-plain.png $PREVIEW_DIR/layout/plain.png
+  convert -resize  500x355 layout-plain.png $PREVIEW_DIR/layout/plain.png
+  convert -resize  750x533 layout-plain.png $PREVIEW_DIR/layout/plain-1.5x.png
+  convert -resize 1000x710 layout-plain.png $PREVIEW_DIR/layout/plain-2x.png
   cat $base.time
 
   echo -n "Side index layout preview ..."
+  base="layout-side-index"
   echo "ocitysmap --config=/home/maposmatic/.ocitysmap.conf --bounding-box=$BBOX --title='Side Index' --format=png --prefix=layout-side-index --language=de_DE.utf8 --layout=single_page_index_side --orientation=landscape --paper-format='$PAPER' --style=CartoOSM" > $base.sh
   chmod a+x $base.sh
   /usr/bin/time -q -f "%E" -o $base.time ./$base.sh > $base.log 2> $base.err
-  convert -resize 500x355 layout-side-index.png $PREVIEW_DIR/layout/single_page_index_side.png
   cat $base.time
+  convert -resize  500x355 layout-side-index.png $PREVIEW_DIR/layout/single_page_index_side.png
+  convert -resize  755x533 layout-side-index.png $PREVIEW_DIR/layout/single_page_index_side-1.5x.png
+  convert -resize 1000x710 layout-side-index.png $PREVIEW_DIR/layout/single_page_index_side-2x.png
 
   echo -n "Bottom index layout preview ..."
+  base="layout-bottom-index"
   echo "ocitysmap --config=/home/maposmatic/.ocitysmap.conf --bounding-box=$BBOX --title='Bottom Index' --format=png --prefix=layout-bottom-index --language=de_DE.utf8 --layout=single_page_index_bottom --orientation=landscape --paper-format='$PAPER' --style=CartoOSM" > $base.sh
   chmod a+x $base.sh
   /usr/bin/time -q -f "%E" -o $base.time ./$base.sh > $base.log 2> $base.err
-  convert -resize 500x355 layout-bottom-index.png $PREVIEW_DIR/layout/single_page_index_bottom.png
   cat $base.time
+  convert -resize  500x355 layout-bottom-index.png $PREVIEW_DIR/layout/single_page_index_bottom.png
+  convert -resize  750x533 layout-bottom-index.png $PREVIEW_DIR/layout/single_page_index_bottom-1.5x.png
+  convert -resize 1000x710 layout-bottom-index.png $PREVIEW_DIR/layout/single_page_index_bottom-2x.png
 
   echo -n "Multi page layout preview ..."
+  base="layout-multi"
   echo "ocitysmap --config=/home/maposmatic/.ocitysmap.conf --bounding-box=$BBOX --title='Multi Page' --format=pdf --prefix=layout-multi --language=de_DE.utf8 --layout=multi_page --orientation=portrait --paper-format='$PAPER' --style=CartoOSM" > $base.sh
   chmod a+x $base.sh
   /usr/bin/time -q -f "%E" -o $base.time ./$base.sh > $base.log 2> $base.err
-  convert -resize 500x355 layout-plain.png $PREVIEW_DIR/layout/single_page_index_bottom.png
   cat $base.time
 
   convert -density 300 layout-multi.pdf layout-multi.png
@@ -83,10 +96,38 @@ else
             layout-multi-index.png     -geometry  +300+75  -composite \
             layout-multi-detail.png    -geometry  +200+50  -composite \
             layout-multi-overview.png  -geometry  +100+25  -composite \
-            layout-multi-title.png     -geometry  +0+0  -composite \
+            layout-multi-title.png     -geometry  +0+0     -composite \
           layout-multi-all.png
 
   cp layout-multi-all.png $PREVIEW_DIR/layout/multi_page.png
+
+  convert layout-multi-0.png  -resize 300x420 layout-multi-title-1.5x.png
+  convert layout-multi-2.png  -resize 300x420 layout-multi-overview-1.5x.png
+  convert layout-multi-5.png  -resize 300x420 layout-multi-detail-1.5x.png
+  convert layout-multi-10.png -resize 300x420 layout-multi-index-1.5x.png
+
+  convert -size 750x533 canvas:white \
+            layout-multi-index-1.5x.png     -geometry  +450+113 -composite \
+            layout-multi-detail-1.5x.png    -geometry  +300+75  -composite \
+            layout-multi-overview-1.5x.png  -geometry  +150+37  -composite \
+            layout-multi-title-1.5x.png     -geometry  +0+0     -composite \
+          layout-multi-all-1.5x.png
+
+  cp layout-multi-all-1.5x.png $PREVIEW_DIR/layout/multi_page-1.5x.png
+
+  convert layout-multi-0.png  -resize 400x560 layout-multi-title-2x.png
+  convert layout-multi-2.png  -resize 400x560 layout-multi-overview-2x.png
+  convert layout-multi-5.png  -resize 400x560 layout-multi-detail-2x.png
+  convert layout-multi-10.png -resize 400x560 layout-multi-index-2x.png
+
+  convert -size 1000x710 canvas:white \
+            layout-multi-index-2x.png     -geometry  +600+150  -composite \
+            layout-multi-detail-2x.png    -geometry  +400+100  -composite \
+            layout-multi-overview-2x.png  -geometry  +200+50  -composite \
+            layout-multi-title-2x.png     -geometry  +0+0     -composite \
+          layout-multi-all-2x.png
+
+  cp layout-multi-all-2x.png $PREVIEW_DIR/layout/multi_page-2x.png
 fi
 
 for style in $STYLES
