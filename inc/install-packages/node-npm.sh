83c6cb5178fb29069fe0a7f801bcf8cfe4fe77f5
Hartmut Holzgraefe <hartmut@php.net>
2022-10-12 23:23:56 +0200

    silence npm install step a bit

--- inc/install-packages/node-npm.sh
+++ inc/install-packages/node-npm.sh
@@ -2,9 +2,12 @@
 
 # install extra npm packages
 banner "npm packages"
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
+
+( curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash - ) > /dev/null
+
+sudo apt-get install --quiet=2 --assume-yes nodejs
 
 npm config set loglevel warn
 
 npm install -g carto > /dev/null || exit 3
+
