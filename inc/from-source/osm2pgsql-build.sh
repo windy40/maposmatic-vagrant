e131f64168e0a76b03ae5f4e2e87cbbc26a92c76
Hartmut Holzgraefe <hartmut@php.net>
2022-10-12 21:50:25 +0200

    no extra buildtool install needed anymore
    all actual dependencies are already handled by apt install script
    
    the build-dep dependencies that were pulled in here are not needed
    as we do not plan to build a debian package, just the binary

--- inc/from-source/osm2pgsql-build.sh
+++ inc/from-source/osm2pgsql-build.sh
@@ -5,8 +5,6 @@ cd $INSTALLDIR
 mkdir -p tools
 cd tools
 
-apt-get build-dep -y osm2pgsql
-
 git clone --quiet https://github.com/openstreetmap/osm2pgsql.git
 
 cd osm2pgsql
@@ -14,5 +12,6 @@ cd osm2pgsql
 mkdir _build
 cd _build
 
-cmake .. >/dev/null
-make -j$(nproc) install >/dev/null
+cmake .. >/dev/null && make -j$(nproc) install >/dev/null
+
+exit 3
