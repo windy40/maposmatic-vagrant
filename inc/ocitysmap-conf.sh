1d18ee3d562ed6f6a2f205f7c257e465bf8f0f46
Hartmut Holzgraefe <hartmut@php.net>
2023-02-26 15:25:31 +0100

    make sure OSM Carto style is default suggestion by sort hack

--- inc/ocitysmap-conf.sh
+++ inc/ocitysmap-conf.sh
@@ -3,6 +3,8 @@
 CONF=${INSTALLDIR:-/home/maposmatic}/.ocitysmap.conf
 INCDIR=${INCDIR:-/vagrant/inc}
 
+# header part is constant
+# TODO: read from /vagrant/files/... instead?
 cat <<EOF > $CONF
 [datasource]
 host=localhost
@@ -27,17 +29,25 @@ US letter= 216x279
 font-path=/usr/share/fonts/:/usr/local/share/fonts/
 EOF
 
+
+# extract list of available base style names
 echo -n "available_stylesheets= " >> $CONF
-grep --no-filename '\[.*\]' $INCDIR/styles/*.ini | sed -e 's/\[//g' -e 's/\]//g' | paste -sd "," >> $CONF
+grep --no-filename '\[.*\]' $INCDIR/styles/*.ini | sed -e 's/\[//g' -e 's/\]//g' | sort | paste -sd "," >> $CONF
 
+# extract list of available overlay style names
 echo -n "available_overlays= " >> $CONF
-grep --no-filename '\[.*\]' $INCDIR/overlays/*.ini | sed -e 's/\[//g' -e 's/\]//g' | paste -sd "," >> $CONF
+grep --no-filename '\[.*\]' $INCDIR/overlays/*.ini | sed -e 's/\[//g' -e 's/\]//g' | sort | paste -sd "," >> $CONF
 
+# separator
+echo >> $CONF
+
+# copy all prepared style .ini sections
 for style_ini in  $INCDIR/styles/*.ini $INCDIR/overlays/*.ini 
 do
 	sed -e 's|@STYLEDIR@|'$STYLEDIR'|g' -e 's|@INSTALLDIR@|'$INSTALLDIR'|g' < $style_ini >> $CONF
 done
 
+# cleanup
 rm -f /root/.ocitysmap.conf
 ln -s $CONF /root/.ocitysmap.conf
 
