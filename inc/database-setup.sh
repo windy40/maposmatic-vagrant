a2b6e16897c0160ae0e9a1def7baaff53861505d
Hartmut Holzgraefe <hartmut@php.net>
2024-03-26 11:40:00 +0100

    create maposmatic DB a bit earlier to ensure it is present when needed

--- inc/database-setup.sh
+++ inc/database-setup.sh
@@ -28,6 +28,9 @@ sudo -u postgres createuser -g maposmatic www-data
 # creade database for osm2pgsql import
 sudo --user=postgres createdb --encoding=UTF8 --locale=en_US.UTF-8 --template=template0 --owner=maposmatic gis
 
+# creade database for maposmatic django frontend
+sudo --user=postgres createdb --encoding=UTF8 --locale=en_US.UTF-8 --template=template0 --owner=maposmatic maposmatic
+
 # set up PostGIS for osm2pgsql database
 sudo --user=postgres psql --dbname=gis --command="CREATE EXTENSION postgis"
 sudo --user=postgres psql --dbname=gis --command="ALTER TABLE geometry_columns OWNER TO maposmatic"
@@ -43,9 +46,6 @@ sudo --user=maposmatic psql --dbname=maposmatic --command="CREATE EXTENSION dbli
 # set up maposmatic admin table
 sudo --user=maposmatic psql --dbname=gis --command="CREATE TABLE maposmatic_admin (last_update timestamp)"
 sudo --user=maposmatic psql --dbname=gis --command="INSERT INTO maposmatic_admin VALUES ('1970-01-01 00:00:00')"
-
-# creade database for maposmatic
-sudo --user=postgres createdb --encoding=UTF8 --locale=en_US.UTF-8 --template=template0 --owner=maposmatic maposmatic
 
 # set password for gis database user
 sudo --user=maposmatic psql --dbname=postgres --command="ALTER USER maposmatic WITH PASSWORD 'secret';"
