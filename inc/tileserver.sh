43c4afbc8536fc428bd6d327fd441da775c0e275
Hartmut Holzgraefe <hartmut@php.net>
2024-04-06 17:30:21 +0200

    finish local tile server configuration

--- inc/tileserver.sh
+++ inc/tileserver.sh
@@ -2,6 +2,9 @@
 
 cat $FILEDIR/config-files/renderd.conf >> /etc/renderd.conf
 
+cp $FILEDIR/config-files/a2-renderd-module.conf /etc/apache2/conf-available/renderd.conf
+a2enconf renderd
+
 mkdir -p /var/lib/mod_tile/osmcarto
 chown _renderd /var/lib/mod_tile/osmcarto
 
