3831771c42a499acefa6dfc62bd8a0ddcc8e2e37
Hartmut Holzgraefe <hartmut@php.net>
2024-04-06 15:20:58 +0200

    use new backup site download URL
    see also:
    
    * https://github.com/gravitystorm/openstreetmap-carto/issues/4864
    * https://github.com/gravitystorm/openstreetmap-carto/pull/4893

--- inc/fonts/hanazono.sh
+++ inc/fonts/hanazono.sh
@@ -5,10 +5,7 @@ FONTDIR=/usr/local/share/fonts/truetype/hanazono
 mkdir -p $FONTDIR
 
 cd $DOWNLOADDIR
-# the site had let its certificate expire multiple times
-# so we won't check for that here
-# see also https://github.com/gravitystorm/openstreetmap-carto/issues/4864
-wget --timestamping --no-check-certificate https://osdn.net/frs/redir.php?f=hanazono-font%2F68253%2Fhanazono-20170904.zip -O hanazono.zip
+wget --timestamping 'https://mirrors.dotsrc.org/osdn/hanazono-font/68253/hanazono-20170904.zip' -O hanazono.zip
 
 cd $FONTDIR
 unzip -oqq "$DOWNLOAD_DIR/hanazono.zip" HanaMinA.ttf HanaMinB.ttf
